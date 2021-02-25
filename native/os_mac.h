b69cd597b887fdedd5f5befa5f42a0a454e90f46
Manatsawin Hanmongkolchai <git@whs.in.th>
2021-02-26 00:59:42 +0700

    mac: fixed wrong assert, use typedef for c++/objc type

--- native/os_mac.h
+++ native/os_mac.h
@@ -1,17 +1,20 @@
 #pragma once
+#include "stdint.h"
 #ifdef __OBJC__
 #include <Cocoa/Cocoa.h>
 #endif
 
 #define DEFAULT_OSRAWWINDOW {.winid=0}
+#ifdef __OBJC__
+typedef NSView view_t;
+#else
+typedef void view_t;
+#endif
+
 
 typedef union NativeView {
 	uintptr_t winid;
-#ifdef __OBJC__
-	NSView* wnd;
-#else
-	void* wnd;
-#endif
+	view_t* wnd;
 } NativeView;
 
 typedef NativeView OSRawWindow;
