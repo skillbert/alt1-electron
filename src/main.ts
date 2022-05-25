d0cece8e6a5794feb19c729250ffbc5b208f42ba
Adam <classygopher@gmail.com>
2022-05-25 19:57:25 +0100

    fix electron not showing up sometimes

--- src/main.ts
+++ src/main.ts
@@ -103,8 +103,8 @@ class ManagedWindow {
 			height: posrect.height,
 			transparent: true,
 			fullscreenable: false,
+			resizable: false,  // Disables CSS resizing - we do JS-based resizing for now
 			skipTaskbar: true,
-			resizable: process.platform != "linux", // doesn't work as intended on linux; electron is trashware 🤡
 			minimizable: false,
 			maximizable: false,
 			show: false,
@@ -134,7 +134,10 @@ class ManagedWindow {
 		
 		// NOTE: it's very very VERY important that the window must be created with `show: false` and that window.show()
 		// must be called AFTER creating the OSWindowPin. This allows us to manipulate the window before the WM mangles it.
-		this.window.once('ready-to-show', () => { this.window.show(); });
+		this.window.once('ready-to-show', () => {
+			this.window.show();
+			this.windowPin.synchPosition(); // fixes electron not showing up sometimes
+		});
 		managedWindows.push(this);
 	}
 }
